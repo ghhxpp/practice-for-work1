6ea282f8786e35e81fdf9e2c5a9c173175207fe3
ghhxpp <xupengpeng182@126.com>
2017-09-19 12:17:41 +0800

    build 01

---
+++ 01随机把8个老师分配到3个办公室.py
@@ -0,0 +1,24 @@
+# coding :utf-8 
+
+import random
+
+def t_to_o(t,o):
+    office = []
+    for i in range(o):
+        office.append([])
+    for i in range(1,t+1):
+        random.choice(office).append(i)
+    return office    
+
+print(t_to_o(8, 3))
+
+def t_to_o2():
+    office = [[],[],[]]
+    teachers = ['a','b','c','d','e','f','g','h']
+    for teacher in teachers:
+        index = random.randint(0,2)
+        office[index].append(teacher)
+    for i in range(len(office)):
+        print("办公室%d的人数是:%d\n%s"%(i+1,len(office[i]),''.join(office[i])))
+t_to_o2()    
+    
